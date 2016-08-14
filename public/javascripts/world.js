b33cd05bddf1fe1ebfa1ff5a37126826f197e32c
Code Factor <codefactor@yahoo.com>
2016-08-14 10:44:07 -0700

    World starter file.

---
+++ public/javascripts/world.js
@@ -0,0 +1,10 @@
+var world = {
+    blocks: [{
+        id: 0,
+        name: 'stone',
+        assets: ['/images/PlanetCute PNG/Stone Block.png']
+    }],
+    tiles: {
+        '0,0': {}
+    }
+};
