a6f1ff0cd6000f60c3b23d3dc08fcf7f96cda16c
Code Factor <codefactor@users.noreply.github.com>
2016-08-29 19:16:27 -0700

    Using process.env.PORT

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 
+app.set('port', (process.env.PORT || 5000));
+
 //var io = require('socket.io')(http);
 //io.on('connection', function(socket) {
 //	socket.emit('world init', metadata);
@@ -9,6 +11,6 @@ var http = require('http').Server(app);
 
 app.use(express.static('public'));
 
-http.listen(3000, function() {
-	console.log('listening on *:3000');
+http.listen(app.get('port'), function() {
+    console.log('listening on *:' + app.get('port'));
 });
